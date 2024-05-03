d6c0fee9ca94d2a5db73a3077c1e405513e720f5
Vedant <birlavedant13@gmail.com>
2024-05-03 16:10:31 +0530

    done some minor changes

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
-// import XSvg from "../../../components/svgs/X";
-
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
-import XSvg from "../../../components/svgs/X";
+import Sharevibe from "../../../components/svgs/logo.png"
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import toast from "react-hot-toast";
 
 const LoginPage = () => {
 	const queryClient = useQueryClient()
@@ -54,11 +50,12 @@ const LoginPage = () => {
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
 			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
+				<img src={Sharevibe} alt="logo" />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
+					<img src={Sharevibe} alt="logo" className='w-24  lg:hidden fill-white' />
+
 					<h1 className='text-4xl font-extrabold text-white'>{"Let's"} go.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdOutlineMail />
--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import { Link, json } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { useState } from "react";
 
 
@@ -6,7 +6,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
-import XSvg from "../../../components/svgs/X";
+import Sharevibe from "../../../components/svgs/logo.png"
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
@@ -66,11 +66,13 @@ const SignUpPage = () => {
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
 			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className=' lg:w-2/3 fill-white' />
+				<img src={Sharevibe} alt="logo" />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
+					{/* <XSvg className='w-24 lg:hidden fill-white' /> */}
+				<img src={Sharevibe} alt="logo" className='w-24 lg:hidden fill-white' />
+
 					<h1 className='text-4xl font-extrabold text-white'>Join today.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdOutlineMail />
